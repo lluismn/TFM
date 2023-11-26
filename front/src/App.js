1500351b3e36f751c20a9eb8d8c8f72849ccab4f
lluismn <lluismnjr@gmail.com>
2023-11-26 17:06:39 +0100

    TFM

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Untitled</a>
+      </header>
+
+      <main>Lista de productos</main>
+    </div>
+  );
+}
+
+export default App;
